455d0c650feef889e62e0d03d1709aceac1e0802
itsivag <sivacbrf2@gmail.com>
2026-02-11 15:27:42 +0530

    Initial commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,25 @@
+package com.suprbeta
+
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.json.Json
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureHTTP()
+    configureRouting()
+}
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json(Json {
+            ignoreUnknownKeys = true
+            prettyPrint = true
+        })
+    }
+}
---
+++ src/main/kotlin/HTTP.kt
@@ -0,0 +1,35 @@
+package com.suprbeta
+
+import com.asyncapi.kotlinasyncapi.context.service.AsyncApiExtension
+import com.asyncapi.kotlinasyncapi.ktor.AsyncApiPlugin
+import io.ktor.openapi.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.defaultheaders.*
+import io.ktor.server.plugins.openapi.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureHTTP() {
+    install(AsyncApiPlugin) {
+        extension = AsyncApiExtension.builder {
+            info {
+                title("Sample API")
+                version("1.0.0")
+            }
+        }
+    }
+    install(DefaultHeaders) {
+        header("X-Engine", "Ktor") // will send this header with each response
+    }
+    routing {
+        openAPI(path = "openapi") {
+            info = OpenApiInfo(title = "My API", version = "1.0.0")
+        }
+    }
+    routing {
+        swaggerUI(path = "openapi") {
+            info = OpenApiInfo(title = "My API", version = "1.0.0")
+        }
+    }
+}
