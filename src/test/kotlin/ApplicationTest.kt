66e615093a637ed875b7dde0a195c35bb7c0ed75
itsivag <sivacbrf2@gmail.com>
2026-02-14 12:51:34 +0530

    Fix ApplicationTest import error

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -3,7 +3,6 @@ package com.suprbeta
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import module
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
